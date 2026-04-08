16b0fbe94eab9d90ff0300c994715f75afcc6847
Reader <read2016@qq.com>
2026-04-08 12:26:56 +0800

    修改API秘钥写法

--- 01_helloworld/LangChainV1.2.py
+++ 01_helloworld/LangChainV1.2.py
@@ -2,11 +2,11 @@ import os
 from langchain.chat_models import init_chat_model
 from dotenv import load_dotenv
 
-my_key = "sk-07fcbafef536494cbab9765b12b97820"
+
 model = init_chat_model(
     model = "qwen-plus",
     model_provider = "openai",
-    api_key = my_key,
+    api_key = os.getenv("DASHSCOPE_API_KEY"),
     base_url = "https://dashscope.aliyuncs.com/compatible-mode/v1", # SDK 1.2.0 版本新增参数，兼容旧版本 SDK 的接口地址，默认值为 "https://dashscope.aliyuncs.com/compatible-mode/v1"
 )
 
@@ -16,7 +16,7 @@ load_dotenv(encoding='utf-8')
 model2 = init_chat_model(
     model = "deepseek-v3",
     model_provider = "openai",
-    api_key = my_key,
+    api_key = os.getenv("DASHSCOPE_API_KEY"),
     base_url = "https://dashscope.aliyuncs.com/compatible-mode/v1",
 )
 
